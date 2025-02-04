d9f26bbb134fdfdcb24a9333dd878e5206120e88
kinghorey <oreoluwaakinbo.oa@gmail.com>
2025-02-04 16:38:31 +0100

    implemented user caching

--- utils/fetch_fun_fact.py
+++ utils/fetch_fun_fact.py
@@ -1,15 +1,20 @@
 import httpx
 
+fun_fact_cache = {}
 
-def get_fun_fact(x: int):
-    """_summary_
 
-        Args:
-                        x (int): _description_
+def get_fun_fact(x: int) -> str:
+    """
+    Function to get a fun fact about a number.
+    Args:
+        x: int: Number for which to get the fun fact.
+    Returns:
+        str: Fun fact about the number.
+    """
+    if x in fun_fact_cache:
+        return fun_fact_cache[x]
 
-        Returns:
-                        str: _description_
-        """
     url = f"http://numbersapi.com/{x}/math"
     response = httpx.get(url)
+    fun_fact_cache[x] = response.text
     return response.text
--- utils/sum_digits.py
+++ utils/sum_digits.py
@@ -1,19 +1,15 @@
+cache = {}
+
+
 def sum_digits(x: int) -> int:
     """
-            Function to get the sum of the digits of a number.
-            Args:
-                    x: int: Number whose digits are to be summed.
+    Function to get the sum of the digits of a number.
+    Args:
+        x: int: Number whose digits are to be summed.
     """
-    # Initialize sum to 0
-    sum = 0
+    if x in cache:
+        return cache[x]
 
-    # Iterate over the digits of the number
-    while x != 0:
-        # Add the last digit to the sum
-        sum += x % 10
-
-        # Remove the last digit
-        x = x // 10
-
-    # Return the sum of the digits
-    return sum
+    result = sum(int(digit) for digit in str(x))
+    cache[x] = result
+    return result
